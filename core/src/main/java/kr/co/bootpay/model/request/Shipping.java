b312cf71640612869dfafb8573c9556c7cf48033
ehowlsla <rupy1014@gmail.com>
2022-06-16 18:00:38 +0900

    에스크로 추가, enum 추가

--- core/src/main/java/kr/co/bootpay/model/request/Shipping.java
+++ core/src/main/java/kr/co/bootpay/model/request/Shipping.java
@@ -4,5 +4,6 @@ public class Shipping {
     public String receiptId; // 부트페이에서 발급한 영수증 id
     public String deliveryCorp; // 택배 회사명
     public String trackingNumber; // 송장번호
-
+    public ShippingUser user;
+    public ShippingCompany company;
 }
