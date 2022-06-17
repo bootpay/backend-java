de553745a778f9a92b11d6603329ab116409bdd8
ehowlsla <rupy1014@gmail.com>
2022-06-17 15:52:23 +0900

    model update

--- core/src/main/java/kr/co/bootpay/model/request/Item.java
+++ core/src/main/java/kr/co/bootpay/model/request/Item.java
@@ -4,7 +4,7 @@ public class Item {
     public String name; // 상품명
     public int qty; // 수량
     public String id; // 상품 고유키
-    public long price; // 상품단가
+    public double price; // 상품단가
     public String cat1; // 카테고리 상
     public String cat2; // 카테고리 중
     public String cat3; // 카테고리 하
--- core/src/main/java/kr/co/bootpay/model/request/SubscribePayload.java
+++ core/src/main/java/kr/co/bootpay/model/request/SubscribePayload.java
@@ -8,14 +8,14 @@ public class SubscribePayload {
     public String billingKey; // 발급받은 빌링키
     public String orderName; // 결제할 상품명
     public String orderId; // 개발사에서 지정하는 고유주문번호
-    public long price; // 결제할 상품금액
-    public int taxFree; // 면세 상품일 경우 해당만큼의 금액을 설정
+    public double price; // 결제할 상품금액
+    public double taxFree; // 면세 상품일 경우 해당만큼의 금액을 설정
     public int cardQuota; // 5만원 이상 결제건에 적용하는 할부개월수. 0-일시불, 1은 지정시 에러 발생함, 2-2개월, 3-3개월... 12까지 지정가능
     public int cardInterest; // 웰컴페이먼츠 전용, 무이자여부를 보내는 파라미터가 있다
     public User user; // 구매자 정보, 특정 PG사의 경우 구매자 휴대폰 번호를 필수로 받는다
     public List<Item> items; // 구매할 상품정보, 통계용
     public String feedbackUrl; // webhook 통지시 받으실 url 주소 (localhost 사용 불가)
-    public String feedbackContentType; // webhook 통지시 받으실 데이터 타입 (json 또는 urlencoded, 기본값 urlencoded)
+    public String contentType; // webhook 통지시 받으실 데이터 타입 (application/json 또는 application/x-www-form-urlencoded 선택)
     public Map<String, Object> metadata = new HashMap<>();
     public SubscribeExtra extra;
 //    public String schedulerType; //정기결제 예약시 - oneshot 으로 지정해야함.
